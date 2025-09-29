274862872ff3e74d0c95f59dfeafea507cdb4280
laura <laura.carbajal9912@alumnos.udg.mx>
2025-09-28 18:14:00 -0600

    quizasmente

--- madre/backend/profesores.sql
+++ madre/backend/profesores.sql
@@ -1,7 +1,8 @@
 CREATE TABLE profesores (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    materia VARCHAR(100) NOT NULL,
+    perfil VARCHAR(100) NOT NULL,
     horario VARCHAR(100) NOT NULL,
-    contrasenia VARCHAR(100) NOT NULL
+    contrasenia VARCHAR(12) NOT NULL,
+    contacto VARCHAR(12) NOT NULL
 );
